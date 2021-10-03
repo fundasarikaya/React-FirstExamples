73b8505afb450196dc2e93515bc11f68c28faa3d
fundasarikaya <fundamelihcan@gmail.com>
2021-10-03 23:06:04 +0300

    Added Components

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,11 +1,25 @@
-
+import { Container, Row, Col } from "reactstrap";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
 
 function App() {
+  let titleCategory = "Category List";
   return (
     <div>
-      <h2>
-        Hello React :D
-      </h2>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList title={titleCategory} />
+          </Col>
+          <Col xs="3">
+            <ProductList title="Product List" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
